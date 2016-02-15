4e36ba33843df1b645ab0e735b5d1c60b8508833
WaqarMobeen <waqarmobeen@deltashoppe.com>
2016-02-15 17:36:09 +0500

    Top Navigation Fix + Added New Font Face

--- js/custom.js
+++ js/custom.js
@@ -10,15 +10,25 @@
         {
         	var menuPosition = $('#services').offset().top;
 	          if ( $(window).scrollTop() >= menuPosition ){
-    	      	$('nav').removeClass('no-display');
+    	      	$('nav').removeClass('no-display').addClass('navbar navbar-static-top navbar-inverse fixed');
         	    }else{
-            	$('nav').addClass('no-display');
+            	$('nav').removeClass('navbar navbar-static-top navbar-inverse fixed').addClass('no-display');
             	}
         };
 
-$('#btn-hire-me').click(function (e) {
+$('#btn-services').click(function (e){
+    e.preventDefault();
     $('html, body').animate(
     {scrollTop: $('#services').offset().top},
+    {duration: 1000,easing: "swing"}
+    );
+});
+
+
+$('#btn-home').click(function (e){
+    e.preventDefault();
+    $('html, body').animate(
+    {scrollTop: $('#header').offset().top},
     {duration: 1000,easing: "swing"}
     );
 });
